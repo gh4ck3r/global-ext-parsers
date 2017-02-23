1a123ef3e9b9657f3cf6bba12f6e97ef7047b00c
Changbin Park <changbin.park@intel.com>
2017-02-23 17:52:46 +0900

    Add testcases for ForStatement

--- test/020.ForStatement.js
+++ test/020.ForStatement.js
@@ -2,4 +2,6 @@
 for (let i=0;i<10;  i); // i is ForStatement.update
 for (let i=0;i<10; +i); // i is UnaryExpression.argument
 for (let i=0;i<10;++i); // i is UpdateExpression.argument
+let i=0;
+for (i=0;i<10;++i);
 // ignore variable defined in for statement
