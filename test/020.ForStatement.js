5367ea67399b18ec682f8add7401b6d9e67ade46
Changbin Park <changbin.park@intel.com>
2017-02-23 16:03:45 +0900

    Revise some test cases

--- test/020.ForStatement.js
+++ test/020.ForStatement.js
@@ -1,3 +1,5 @@
 "use strict";
-for (let i=0;i<10;i);
+for (let i=0;i<10;  i); // i is ForStatement.update
+for (let i=0;i<10; +i); // i is UnaryExpression.argument
+for (let i=0;i<10;++i); // i is UpdateExpression.argument
 // ignore variable defined in for statement
