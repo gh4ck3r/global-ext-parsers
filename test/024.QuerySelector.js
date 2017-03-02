70a5ba60d44c624ef6044fe64f332e0757e6015d
Changbin Park <changbin.park@intel.com>
2017-03-02 14:37:38 +0900

    Add test for supporting querySelector API

---
+++ test/024.QuerySelector.js
@@ -0,0 +1,8 @@
+"use strict";
+document.querySelector("div.class-a");
+document.querySelector("div.class-a.class-b");
+document.querySelector("div.user-panel.main input[name=login]");
+document.querySelector(`
+    div.class-a
+    div.class-b
+`);
